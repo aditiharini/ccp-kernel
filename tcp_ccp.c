345615b26ab3e9dfcdbcd057e37df68d084bc819
Aditi Srinivasan <aditiharini@gmail.com>
2019-05-19 01:28:19 +0000

    working rust prog

--- tcp_ccp.c
+++ tcp_ccp.c
@@ -24,7 +24,6 @@
 #define CCP_FRAC_DENOM 10
 #define CCP_EWMA_RECENCY 6
 
-static int count;
 
 void ccp_set_pacing_rate(struct sock *sk, uint32_t rate) {
     sk->sk_pacing_rate = rate;
@@ -265,11 +264,9 @@ void tcp_ccp_cong_control(struct sock *sk, const struct rate_sample *rs) {
             return;
         }
 		
-		count++;	
 		bpf_data[0] = rs->delivered;
 		bpf_data[1] = (s32) rs->rtt_us;
-		//bpf_data[2] = rs->losses;
-		bpf_data[2] = count;
+		bpf_data[2] = rs->losses;
 		bpf_data[3] = rs->acked_sacked;
 
 		int ret = tcp_call_bpf(sk, BPF_SOCK_OPS_TCP_CONNECT_CB, 4, bpf_data);
--- user/src/main.rs
+++ user/src/main.rs
@@ -14,7 +14,17 @@ use std::mem;
 static mut perf_fds: [libc::c_int; 4] = [0; 4];
 
 unsafe extern "C" fn print_output(data: *mut libc::c_void, size: libc::c_int) -> bpf_perf_event_ret {
-    print!("got here");
+    struct Data {
+        delivered: i32,
+        rtt_us: i32,
+        losses: i32,
+        acked_sacked: u32,
+    };
+    let data_s = data as *mut Data;
+    println!("delivered: {}", (*data_s).delivered);
+    println!("rtt_us: {}", (*data_s).rtt_us);
+    println!("losses: {}", (*data_s).losses);
+    println!("acked_sacked: {}", (*data_s).acked_sacked);
     return bpf_perf_event_ret_LIBBPF_PERF_EVENT_CONT;
    
 }
@@ -51,8 +61,13 @@ fn init() {
             __reserved_2: 0,
         };
         for i in 0..4 {
-            perf_fds[i] = libc::syscall(libc::SYS_perf_event_open, &_attr, i, 0, -1, 0) as libc::c_int;
-            bpf_map_update_elem(map_fd[0], &i as *const _ as *const _, &perf_fds[i] as *const _ as *const _, BPF_ANY as u64);
+            perf_fds[i] = libc::syscall(libc::SYS_perf_event_open, &_attr, -1, i, -1, 0) as libc::c_int;
+            if perf_fds[i] < 0 {
+                print!("fd init issue");
+            }
+            if bpf_map_update_elem(map_fd[0], &i as *const _ as *const _, &perf_fds[i] as *const _ as *const _, BPF_ANY as u64) != 0 {
+                print!("map init issue");
+            }
             ioctls::perf_event_ioc_enable(perf_fds[i]);
         }
 
@@ -61,23 +76,38 @@ fn init() {
 
 fn main() {
     unsafe {
-        let filename = CString::new("../../bpf/bpf_perf_kern.o").expect("CString::new failed");
-        load_bpf_file(filename.as_ptr() as *mut std::os::raw::c_char);
+        let filename = CString::new("../bpf/bpf_perf_kern.o").expect("CString::new failed");
+        if load_bpf_file(filename.as_ptr() as *mut std::os::raw::c_char) != 0 {
+            print!("couldn't load bpf file"); 
+            return;
+        }
         
         let cgrp_name = CString::new("/tmp/cgroupv2/foo").expect("CString::new failed");
         let cg_fd = libc::open(cgrp_name.as_ptr() as *mut std::os::raw::c_char, libc::O_DIRECTORY, libc::O_RDONLY);
+        if cg_fd < 0 {
+            print!("error init cgroup");
+            return;
+        }
 
-        bpf_prog_attach(prog_fd[0], cg_fd, bpf_attach_type_BPF_CGROUP_SOCK_OPS, 0);
+        if bpf_prog_attach(prog_fd[0], cg_fd, bpf_attach_type_BPF_CGROUP_SOCK_OPS, 0) !=  0 {
+            print!("prog attach failed"); 
+            return;
+        }
         
         init();
 
         let mut headers: [*mut perf_event_mmap_page; 4] = mem::uninitialized(); 
 
+        println!("got to mmap");
+
         for i in 0..4 {
-            perf_event_mmap_header(perf_fds[i], &mut headers[i]);
+            if perf_event_mmap_header(perf_fds[i], &mut headers[i]) < 0 {
+                print!("mmap error");
+                return;
+            }
         }
 
-
+        println!("got to poll");
         perf_event_poller_multi(perf_fds.as_mut_ptr(), headers.as_mut_ptr(), 4, Option::from(print_output as unsafe extern "C" fn(*mut libc::c_void, i32) -> i32));
 
 
